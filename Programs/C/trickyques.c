5435234b0c0b3392bd3360c8760d0cf0e34659fe
AjayKrChaturvedi <chaturvedikraj2109@gmail.com>
2022-01-22 11:02:54 +0530

    Update trickyques.c

--- Programs/C/trickyques.c
+++ Programs/C/trickyques.c
@@ -1,9 +1,29 @@
 #include<stdio.h>
-
+//printing values without using ;
 void main()
 {
     if(printf("Ajay"))
     {}
     while(!printf("Ajay"))
     {}
+}
+
+arr["even","odd"];
+taken input in num var
+printf("",arr[num%2]);
+
+
+#include <stdio.h>
+//without control statements
+int main()
+{
+int a=6;
+(a%2==0 && printf("Even"));
+(a%2!=0 && printf("Odd"));
+ int i;
+ for(i=1;i<5;i++)
+ {
+     printf("%.*s",i,"*****");
+ }
+
 }
