de7c34e382751642fa489810286cb6af1dea2de2
AjayKrChaturvedi <chaturvedikraj2109@gmail.com>
2022-01-25 08:53:23 +0530

    Updation

---
+++ Programs/C++/Check.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//without control statements
+int main()
+{
+int a=6;
+(a%2==0 && printf("0"));
+(a%2!=0 && printf(""));
+ int i;
+ i=printf(" ");
+ printf("\n%d",i);
+ /*for(i=1;i<5;i++)
+ {
+     printf("%.*s",i,"*****");
+ }*/
+
+}
+
