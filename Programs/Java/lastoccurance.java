3b8c9c531f64fd2a15e40c4d146893e45cefde05
AjayKrChaturvedi <chaturvedikraj2109@gmail.com>
2022-02-25 10:43:31 +0530

    Updation

---
+++ Programs/Java/lastoccurance.java
@@ -0,0 +1,33 @@
+package Java;
+
+import java.util.Scanner;
+
+public class lastoccurance {
+    public static void main(String[] args) {
+		String lastCharStr;
+		int i = 0, lIndex = -1;
+		
+	 Scanner sc= new Scanner(System.in);
+
+		System.out.print("\nPlease Enter String to find Last Occurrence =  ");
+		lastCharStr = sc.nextLine();
+		
+		System.out.print("\nEnter the Last Character to Find =  ");
+		int lsch = sc.next().charAt(0);
+		
+		while(i < lastCharStr.length())
+		{
+			if(lastCharStr.charAt(i) ==  lsch) {
+				lIndex = i;
+			}
+			i++;
+		}
+		if(lIndex == -1) {
+			System.out.println("\nSorry! We haven't found the Character ");
+		}
+		else {
+			System.out.format("\nThe Last Character Occurrence of %c at %d position", lsch, lIndex);
+		}
+        sc.close();
+}
+}
